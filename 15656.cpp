8c2d2eca7ecc5f95aca2f17373c97fd27d5017c2
donghyun1998 <donghyun1998@naver.com>
2023-02-10 00:40:24 +0900

    ok

---
+++ 15656.cpp
@@ -0,0 +1,37 @@
+// n과 m7
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace	std;
+
+int	n, m;
+int	numset[10];
+int	arr[10];
+
+void	recur(int depth)
+{
+	if (depth == m) // 종료조건
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' '; // 수열하나 출력
+		cout << '\n';
+		return ;
+	}
+	for (int i = 0; i < n; i++) // 1부터 n까지 숫자에 대해 반복
+	{
+		arr[depth] = numset[i]; // depth가 수열의 개수만큼
+		recur(depth + 1); // 한 수열에 대해 나머지 수 넣어주고 출력
+	}
+}
+
+int	main()
+{
+	ios::sync_with_stdio(0),cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> numset[i];
+	sort(numset, numset + n);
+	recur(0);
+}
